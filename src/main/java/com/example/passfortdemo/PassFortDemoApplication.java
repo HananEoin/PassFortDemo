41854546046af5ba05ed9e2f06e195a3b1a168d7
Eoin Hanan <eoinhanan@gmail.com>
2021-06-21 13:24:37 +0100

    Initial commit

---
+++ src/main/java/com/example/passfortdemo/PassFortDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.passfortdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassFortDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassFortDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/passfortdemo/PassFortDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passfortdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassFortDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
